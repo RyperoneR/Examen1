7a66201ee8891b26016d3d7e545e0287fc2eac96
RyperoneR <145667779+RyperoneR@users.noreply.github.com>
2023-11-10 14:00:26 +0100

    Update analisistemperaturasemana.py

--- analisistemperaturasemana.py
+++ analisistemperaturasemana.py
@@ -1,9 +1,9 @@
-
+#Creo una función de temperatura media
 def temperatura_media(temperaturas):
     total=sum(temperaturas)
     media=total/7
     return media
-
+#Creo una función para definir el día de máxima temperatura
 def dia_de_maximo(temperaturas):
     temp_max=max(temperaturas)
     if temp_max == temperaturas[0]:
@@ -22,13 +22,13 @@ def dia_de_maximo(temperaturas):
         return "día 7"
     else:
         print("Ha habido un error, intentalo de nuevo")
-
+#Creo una función para especificar si la temperatura de esa semana ha oscilado entre los 20 y los 25 grados
 def mild(temperaturas):
     if 20<=temperaturas[0]<=25 and 20<=temperaturas[1]<=25 and 20<=temperaturas[2]<=25 and 20<=temperaturas[3]<=25 and 20<=temperaturas[4]<=25 and 20<=temperaturas[5]<=25 and 20<=temperaturas[6]<=25:
         print("Esta semana la temperatura ha estado entre los 20 y los 25 grados Celsius.")
     else:
         pass
-
+#Creo una función para ver cuanto ha subido o bajado la temperatura cada día a lo largo de la semana
 def fluctuación(temperaturas):
     día_uno=temperaturas[0]-temperaturas[0]
     día_dos=temperaturas[1]-temperaturas[0]
@@ -94,10 +94,11 @@ def fluctuación(temperaturas):
     else:
         print("La temperatura no ha subido ni ha bajado el día 7.")
 
+#especfico las temperaturas a lo largo de los días de las diferentes semanas
 temperatura_semana_1=[22,24,19,21,25,23,20]
 temperatura_semana_2=[20,22,21,23,24,22,21]
 temperatura_semana_3=[23,21,20,22,24,25,23]
-
+#Escribo los resultados
 print("La temperatura media de la semana uno es de", temperatura_media(temperatura_semana_1),"grados Celsius. El día de máxima temperatura ha sido el", dia_de_maximo(temperatura_semana_1))
 mild(temperatura_semana_1)
 print("La fluctuación en la semana 1 ha sido la siguiente:")
